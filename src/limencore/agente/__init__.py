98347b5e31aedad46b842acbd273119b848d8b71
lucas <jlogb@cesar.school>
2026-09-17 08:41:34 -0300

    update agente/__init__.py

---
+++ src/limencore/agente/__init__.py
@@ -0,0 +1,6 @@
+from .pergunta import CamadaContencao, Pergunta
+from .seletor import SeletorDePergunta, SeletorGenerico
+
+#NanoApi
+
+__all__ = ["CamadaContencao", "Pergunta", "SeletorDePergunta", "SeletorGenerico"] #Diretorios externos puxam daqui
